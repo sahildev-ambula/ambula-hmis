211835ffbdbe1a33d8b83e1ef3646966c3e70c62
Sahil Sharma <144793214+sahildev-ambula@users.noreply.github.com>
2024-03-16 10:19:32 +0530

    web setup

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import {Text, View} from 'react-native';
+
+function App() {
+  return (
+    <View
+      style={{
+        backgroundColor: 'wheat',
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        
+      }}>
+      <Text>Hi, I'm Sahil. Welcome to HMIS</Text>
+    </View>
+  );
+}
+
+export default App;
